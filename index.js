b0ced0e0a8b2547d6c18689342b41cc47785fa00
JoleneMel <jolenemelanson33@gmail.com>
2022-04-27 19:54:28 -0400

    roman numeral converter

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function convertToRoman(num) {
     //defining string 
     let result = "";
-    //using a while loop with compound assignment 
-    while (num >= 0) {
-      //Adding an I to the string
-      result += "I";
-      num -= 1;
-    }
+      //Using the material from https://www.mathsisfun.com/roman-numerals.html we are 
+        //gonna add Vs first so we need another while loop. 
+        while (num >= 4) {
+            result += "IV";
+            //IV 4 so we need to subtract the full amount 
+            num -= 4;
+        }
+        //using a while loop with compound assignment 
+        while (num >= 0) {
+        //Adding an I to the string
+        result += "I";
+        num -= 1;
+        }
    return result;
   }
   
