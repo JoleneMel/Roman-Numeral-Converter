32f7f4586f71459401d85b6b4ecd8f8eb840b695
JoleneMel <jolenemelanson33@gmail.com>
2022-04-27 20:34:02 -0400

    roman numeral converter

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 function convertToRoman(num) {
     //defining string 
-    let result = "";
-    //with 40 or XL
-    while (num >= 40) {
-    result += "XL";
-    num -= 40;
-  }
+      let result = "";
+      //with 1000 or M
+      while (num >= 1000) {
+      result += "M";
+      num -= 1000;
+    }
+       //console.log to track how much is left 
+    console.log(`Finished with M, leftover: ${num}`);
+  
+      //with 900 or D
+      while (num >= 900) {
+      result += "CM";
+      num -= 900;
+    }
+       //console.log to track how much is left 
+    console.log(`Finished with CM, leftover: ${num}`);
+  
+      //with 500 or D
+      while (num >= 500) {
+      result += "D";
+      num -= 500;
+    }
+       //console.log to track how much is left 
+    console.log(`Finished with D, leftover: ${num}`);
+  
+      //with 400 or CD
+      while (num >= 400) {
+      result += "CD";
+      num -= 400;
+    }
+       //console.log to track how much is left 
+    console.log(`Finished with CD, leftover: ${num}`);
+  
+      //with 100 or C
+      while (num >= 100) {
+      result += "C";
+      num -= 100;
+    }
+      //console.log to track how much is left 
+    console.log(`Finished with C, leftover: ${num}`);
+  
+      //with 90 or XC
+      while (num >= 90) {
+      result += "XC";
+      num -= 90;
+    }
+       //console.log to track how much is left 
+    console.log(`Finished with XC, leftover: ${num}`);
+  
+      //with 50 or L
+      while (num >= 50) {
+      result += "L";
+      num -= 50;
+    }
+       //console.log to track how much is left 
+    console.log(`Finished with L, leftover: ${num}`);
+  
+      //with 40 or XL
+      while (num >= 40) {
+      result += "XL";
+      num -= 40;
+    }
+       //console.log to track how much is left 
+    console.log(`Finished with XL, leftover: ${num}`);
+  
     //with 10 or X
       while (num >= 10) {
       result += "X";
@@ -13,6 +72,7 @@ function convertToRoman(num) {
     }
        //console.log to track how much is left 
     console.log(`Finished with X, leftover: ${num}`);
+  
     //This works UNTIL we reach 9 where it is IX
       while (num >= 9) {
       result += "IX";
@@ -20,6 +80,7 @@ function convertToRoman(num) {
     }
        //console.log to track how much is left 
     console.log(`Finished with IX, leftover: ${num}`);
+  
     //We need to go to the next largest one which is 5
     while (num >= 5) {
       result += "V";
@@ -27,6 +88,7 @@ function convertToRoman(num) {
     }
      //console.log to track how much is left 
     console.log(`Finished with V, leftover: ${num}`);
+  
     //Using the material from https://www.mathsisfun.com/roman-numerals.html we are 
     //gonna add Vs first so we need another while loop. 
     //If i is >=4 if less it will be skipped 
@@ -38,6 +100,7 @@ function convertToRoman(num) {
     //using a while loop with compound assignment 
     //console.log to track how much is left 
     console.log(`Finished with IV, leftover: ${num}`);
+  
     while (num >= 1) {
       //Adding an I to the string
       result += "I";
@@ -46,4 +109,4 @@ function convertToRoman(num) {
    return result;
   }
   
-  console.log(convertToRoman(6));
+  console.log(convertToRoman(99));
